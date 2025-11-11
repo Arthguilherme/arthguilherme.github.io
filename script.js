15fae08d1542a2c7af32e311753bd9d6ace03e17
arthguilherme <--replace-all>
2025-11-11 11:03:03 -0300

    feat: feito o javascript

---
+++ script.js
@@ -0,0 +1,16 @@
+const typed = new Typed(".multiple-text", {
+    strings: [
+        "Estudante de Analise e Desenvolvimento de Sistema",
+        "Player de rpg de mesa",
+        "Mestre de RPG de Mesa",
+        "Programador",
+        "Insire aqui",
+        "___________"
+        
+    ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:100,
+    loop: true,
+
+});
