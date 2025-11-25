dc8813941c41c7b7c7703c18d153f95270b0bb24
arthguilherme <arth.ssg@gmail.com>
2025-11-25 09:00:49 -0300

    feat: tema escuro Javascript

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ const typed = new Typed(".multiple-text", {
         "Player de rpg de mesa",
         "Mestre de RPG de Mesa",
         "Programador",
-        "Insire aqui",
-        "___________"
+        "Adramelach",
         
     ],
     typeSpeed:100,
@@ -14,3 +13,37 @@ const typed = new Typed(".multiple-text", {
     loop: true,
 
 });
+const themeToggle = document.getElementById('themeToggle');
+const body  = document.body;
+const themeIcon = document.querySelector('.theme-icon');
+
+function toggleTheme() {
+    body.classList.toggle('light-mode');
+    const isLightMode = body.classList.contains('light-mode');
+
+    if (isLightMode) {
+        themeIcon.className = 'bx bx-moon theme-icon' ;
+        localStorage.setItem('theme','light');
+    } else {
+        themeIcon.className = 'bx bx-sun theme-icon';
+        localStorage.setItem('theme','dark');
+    }
+}
+
+function loadSavedTheme() {
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'light') {
+        body.classList.add('light-mode');
+        themeIcon.className = 'bx bx-moom theme-icon';
+    } else {
+        body.classList.remove('light-mode');
+        themeIcon.className = 'bx bx-sun theme-icon';
+    }
+}
+
+if (document.readyState === 'loading') {
+    document.addEventListener('DomContentLoaded', loadSavedTheme);
+} else {
+    loadSavedTheme();
+}
